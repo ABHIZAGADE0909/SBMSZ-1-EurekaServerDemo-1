6e900bcf6310ca6449a3f73441fa04eec3682170
Lenovo <Lenovo@LAPTOP-MD6ETB0I>
2021-05-16 10:59:18 +0530

    Spring Boot MS Eureka Server

---
+++ src/main/java/com/abhay/sb/SbmsEurekaServerDemo1Application.java
@@ -0,0 +1,15 @@
+package com.abhay.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SbmsEurekaServerDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsEurekaServerDemo1Application.class, args);
+	}
+
+}
